8b07abd2c48ebe7a568dd79bd04e660630070347
Andrew <avanostra@gmail.com>
2024-12-08 16:45:54 -0500

    Updated solution to use BufferedReader to avoid excess syscalls

--- 2024/Day 1/solution.java
+++ 2024/Day 1/solution.java
@@ -1,38 +1,38 @@
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 import java.util.Iterator;
 
 
 public class solution {
     public static void main(String[] args) {
         try{
-            File file = new File("input.txt");
-            Scanner input = new Scanner(file);
+            BufferedReader input = new BufferedReader(new FileReader("input.txt"));
             
             part1(input);
             input.close();
             
-            input = new Scanner(file);
+            input = new BufferedReader(new FileReader("input.txt"));
             part2(input);   
             input.close();
         }
-        catch(FileNotFoundException e){
-            System.out.println("Error reading from file");
-        }        
+        catch(IOException e){
+            System.out.println("Error reading file");
+        }
     }
 
-    public static void part1(Scanner input){
+    public static void part1(BufferedReader input) throws IOException{
         int sum = 0;
         PriorityQueue<Integer> list1 = new PriorityQueue<>();
         PriorityQueue<Integer> list2 = new PriorityQueue<>();
 
         //Read in data
-        while(input.hasNextLine()){
-            String vals[] = input.nextLine().split("   ");
+        String curLine;
+        while((curLine = input.readLine()) != null){
+            String vals[] = curLine.split("   ");
             list1.offer(Integer.parseInt(vals[0]));
             list2.offer(Integer.parseInt(vals[1]));
         }
@@ -44,13 +44,14 @@ public class solution {
         System.out.println(sum);
     }
 
-    public static void part2(Scanner input){
+    public static void part2(BufferedReader input) throws IOException{
         int simScore = 0;
         LinkedList<Integer> list1 = new LinkedList<>(); // To store all the the read in values of list1
         HashMap<Integer, Integer> list2 = new HashMap<>(); //To give quick lookups for list2 counts; 
         
-        while(input.hasNextLine()){
-            String[] vals = input.nextLine().split("   ");
+        String curLine;
+        while((curLine = input.readLine()) != null){
+            String[] vals = curLine.split("   ");
             list1.addLast(Integer.parseInt(vals[0]));
 
             int l2 = Integer.parseInt(vals[1]);
