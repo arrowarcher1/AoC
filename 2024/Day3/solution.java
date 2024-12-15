6f52882546027a653ae6e0b61f87f803bdcc1544
Andrew <avanostra@gmail.com>
2024-12-15 12:47:24 -0500

    We love regex

---
+++ 2024/Day3/solution.java
@@ -0,0 +1,62 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class solution {
+    public static void main(String[] args){
+        try{
+            String input = Files.readString(Paths.get("input.txt"));
+            part1(input);
+            part2(input);
+        }
+        catch(IOException e){
+            System.out.println("Error reading file");
+        }
+
+    }
+
+    public static void part1(String input){
+        String reg = "mul\\(\\d{1,3},\\d{1,3}\\)";
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(input);
+        
+        long total = 0;
+        
+        while(matcher.find()){
+            String currMatch = matcher.group();
+            currMatch = currMatch.replaceAll("mul\\(", "");
+            currMatch = currMatch.replaceAll("\\)", "");
+            String[] vals = currMatch.split(",");
+            
+            total += (Integer.parseInt(vals[0]) * Integer.parseInt(vals[1]));
+        }
+        
+        System.out.println(total);
+    }
+
+    public static void part2(String input){
+        String reg = "mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)";
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(input);
+        boolean enableMUL = true;
+        long total = 0;
+
+        while(matcher.find()){
+            String currMatch = matcher.group();
+            if(currMatch.compareTo("do()") == 0){
+                enableMUL = true;
+            }else if(currMatch.compareTo("don't()") == 0){
+                enableMUL = false;
+            }else if(enableMUL){
+                currMatch = currMatch.replaceAll("mul\\(", "");
+                currMatch = currMatch.replaceAll("\\)", "");
+                String[] vals = currMatch.split(",");
+                total += (Integer.parseInt(vals[0]) * Integer.parseInt(vals[1]));
+            }
+        }
+
+        System.out.println(total);
+    }
+}
